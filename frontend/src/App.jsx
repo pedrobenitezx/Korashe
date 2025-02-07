d66da9ba583518d907744f51f63e4a76aacb00f5
Pedro Benítez <77728916+pedrobenitezx@users.noreply.github.com>
2025-02-07 12:47:45 +0100

    add routing to App component and remove unused merch page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[6vw] lg:px-[8vw]'>
+
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/orders' element={<Orders/>} />
+      <Route path='/product/:productId' element={<Product/>} />
+      <Route path='/placeorder' element={<PlaceOrder/>} />
+      </Routes>
 
     </div>
   )
